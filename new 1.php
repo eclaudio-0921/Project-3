8089f350b85aaa72f6d76a80698b40eddd8fe38f
eclaudio-0921 <143681881+eclaudio-0921@users.noreply.github.com>
2023-11-14 18:55:07 -0600

    adding html pages

---
+++ new 1.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+ <head>
+   <meta charset="UTF-8">
+   <title>Eric Claudio's Resume</title>
+   <link rel="stylesheet" href="css/normalize.css">
+   <link rel="stylesheet" href="css/style.css">
+ </head>
+ <body>
+ <header>
+   <a href="index.html">Resume</a>
+   <a href="page2.html">God of War</a>
+   <a href="page3.html">Super Mario Bros.</a>
+ </header>
+
+ <section>
+   <h1>Eric's Resume</h1>
+     <p>Eric Claudio</p>
+     <p>eclaudio@hawk.iit.edu</p>
+ </section>
+    
+ <section>
+   <h2>Education</h2>
+   <ul>
+     <li>As of now attending the Chicago High School For Agricultural Sciences class of 2024</li>
+     <li>Enrolled in Fudamentals of Web Development as an enrollment course with IIT</li>
+     </ul>
+ </section>
+
+ <section>
+   <h2>Work Experience</h2>
+   <p class="subheading">School & Summer Skills</p>
+   <ul>
+     <li>(NHS) National Honor Society- Service Learning</li>
+     <li>Depaul University-Summer Digital Arts program Location:Loop Campus</li>
+   </ul>
+ </section>
+
+ <section>
+   <h2>Computer Science Skills</h2>
+   <p class="subheading">Computer Programming</p>
+   <ul>
+     <li>CS Course-AP CSA</li>
+     <li>HTML</li>
+     <li>CSS Elements</li>
+   </ul>
+ </section>
+
+    <footer>
+     <p><a href="mailto:eclaudio@hawk.iit.edu"><em>eclaudio@hawk.iit.edu</em></a></p>
+    </footer>
+  </body>
+
+</html>
