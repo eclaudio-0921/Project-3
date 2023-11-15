cd42cf41de6bda97dccce39bea3ad38174e237b2
eclaudio-0921 <143681881+eclaudio-0921@users.noreply.github.com>
2023-11-14 20:28:34 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 function initMap() {
-
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: 41.95535438394767, lng: -87.72210225143667 },
+    zoom: 12,
+  });
 
 
 
