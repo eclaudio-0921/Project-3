77b308cf99a01dda99374ab894b67fe813416239
eclaudio-0921 <143681881+eclaudio-0921@users.noreply.github.com>
2023-12-05 19:04:06 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,21 +6,20 @@ function initMap() {
     zoom: 15
   });
  
-  icon: school {
-url:"media/5b4e0fe274509.image.png"
-},
-  icon: Midway {
-url:"media/38caab_eddba0af63cb4e5b8610cc76f57b735c~mv2.png"
-},
- 
-  var school = new google.maps.Marker({
+var school = new google.maps.Marker({
     position: { lat:  41.69030, lng: -87.71769},
     map: map
+  icon: school {
+  url:"media/5b4e0fe274509.image.png"
+  },
   });
 
   var Midway = new google.maps.Marker({
     position: { lat: 41.78699, lng: -87.75224 },
     map: map
+  icon: Midway {
+  url:"media/38caab_eddba0af63cb4e5b8610cc76f57b735c~mv2.png"
+  },
   });
 
   var distPoints = [
