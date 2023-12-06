b09372b1f3b69d2eb5cee44b149a350c54cbde83
eclaudio-0921 <143681881+eclaudio-0921@users.noreply.github.com>
2023-12-05 18:13:08 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,38 +1,43 @@
-var map;
 function initMap() {
 
   map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: 41.76999521182157, lng: -87.70415171237143 },
-    zoom: 15,
+    //center: {lat: 41.69030, lng: -87.71769}, 
+    center: {lat: 41.6902, lng: -87.7176},
+    zoom: 15
+  });
+const icons = {
+    budlong: {
+      icon: ""
+    },
+    harolds: {
+      icon: ""
+    },
+    flacos: {
+      icon: ""
+    }
+  }
+  var school = new google.maps.Marker({
+    position: { lat:  41.69030, lng: -87.71769},
+    map: map
   });
 
-  const icons = {
-    ChicagoAg: {
-      icon: "media/5b4e0fe274509.image.jpg"
-    },
-    Midway: {
-      icon: "media/38caab_eddba0af63cb4e5b8610cc76f57b735c~mv2.jpg"
-    }  
-  }
+  var Midway = new google.maps.Marker({
+    position: { lat: 41.78699, lng: -87.75224 },
+    map: map
+  });
 
-  const features = [
-    {
-      position: new google.maps.LatLng(41.69030, -87.71769),
-      type: "ChicagoAg",
-    },
-    {
-      position: new google.maps.LatLng( 41.78699, -87.75224),
-      type: "Midway",
-    } 
-  ]
+  var distPoints = [
+    { lat: 41.69030, lng: -87.71769},
+    { lat: 41.78699, lng: -87.75224  },
+  ];
 
-  for (let i = 0; i < features.length; i++) {
-    const marker = new google.maps.Marker({
-      position: features[i].position,
-      icon: icons[features[i].type].icon,
-      map: map,
-    });
-  }
+  var dist = new google.maps.Polyline({
+    path: distPoints,
+    geodesic: true,
+    strokeColor: "#FE5F55",
+    strokeOpacity: 1.0,
+    strokeWeight: 2,
+  });
+  dist.setMap(map);
 }
-
-initMap();
+window.initMap = initMap;
